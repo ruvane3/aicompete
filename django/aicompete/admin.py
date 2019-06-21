ddaa805d14c6a52bc7ae967a12cff56bbe06b0c8
kyler <kyler@KylerDesktop.localdomain>
2019-06-20 23:00:21 -0700

    .gitignore fix

--- django/aicompete/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import GameState
-
-admin.site.register(GameState)
--- django/aicompete/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AiCompete(AppConfig):
-    name = 'aicompete'
